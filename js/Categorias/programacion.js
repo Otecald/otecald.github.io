5b7b2b8a800476eeac327d4f847511addae74e0b
Emiliano Ezequiel Parenti <passymas@gmail.com>
2020-03-19 23:27:27 -0300

    Update programacion.js

--- js/Categorias/programacion.js
+++ js/Categorias/programacion.js
@@ -2,32 +2,32 @@
 	{
 		título:"Chatovod Mod",
 		descripción:"Extensión del Chrome que le da mejoras a Chatovod.",
-		foto:"Javascript/Paginas/Chatovod/Chatovod_Mod/Thumb.png",
-		enlace:"Javascript/Paginas/Chatovod/Chatovod_Mod/Chatovod_Mod.html"
+		foto:"js/Paginas/Chatovod/Chatovod_Mod/Thumb.png",
+		enlace:"js/Paginas/Chatovod/Chatovod_Mod/Chatovod_Mod.html"
 	},{
 		título:"Bot conversacional para Chatovod",
 		descripción:"Bot para Chatovod, OtecaldBot (no funciona)",
-		foto:"Javascript/Paginas/OtecaldBot/Thumb.png",
-		enlace:"Javascript/Paginas/OtecaldBot/Bot.html"
+		foto:"js/Paginas/OtecaldBot/Thumb.png",
+		enlace:"js/Paginas/OtecaldBot/Bot.html"
 	},{
 		título:"Secuencia Binaria",
 		descripción:"Javascript que genera una secuencia binaria irracional única, parecida a pi.",
-		foto:"Javascript/Paginas/Secuencia Binaria/Thumb.png",
-		enlace:"Javascript/Paginas/Secuencia Binaria/Secuencia Binaria.html"
+		foto:"js/Paginas/Secuencia Binaria/Thumb.png",
+		enlace:"js/Paginas/Secuencia Binaria/Secuencia Binaria.html"
 	},{
 		título:"Base 64 hacia palabras de Wikcionario y viceversa.",
 		descripción:"Convierte código base 64 a palabras de Wikcionario y palabras a base 64.",
-		foto:"Javascript/Paginas/Base64_Wikcionario/Thumb.png",
+		foto:"js/Paginas/Base64_Wikcionario/Thumb.png",
 		enlace:"rawgit Base64_hacia_Palabras/HTML.html"
 	},{
 		título:"Diferencia de Fechas",
 		descripción:"Javascript que obtiene la diferencia que hay entre dos fechas.",
-		foto:"Javascript/Paginas/Diferencia%20de%20Fechas/Thumb.png",
-		enlace:"Javascript/Paginas/Diferencia%20de%20Fechas/Diferencia%20de%20fechas.html"
+		foto:"js/Paginas/Diferencia%20de%20Fechas/Thumb.png",
+		enlace:"js/Paginas/Diferencia%20de%20Fechas/Diferencia%20de%20fechas.html"
 	},{
 		título:"Predictor de Quiniela",
 		descripción:"Javascript que intenta mostrar una estadística de la Quiniela.",
-		foto:"Javascript/Paginas/Quiniela/Thumb.png",
+		foto:"js/Paginas/Quiniela/Thumb.png",
 		enlace:"rawgit Quiniela/HTML.html"
 	}
 ]
