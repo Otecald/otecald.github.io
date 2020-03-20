20730c250b933ee0927d04b97a4518441fae2570
ArtEze <passymas@gmail.com>
2020-03-20 03:46:16 -0300

    Cambiar funciones de ocultar y mostrar..

--- js/index/html.js
+++ js/index/html.js
@@ -1,32 +1,58 @@
-﻿function mostrar(objetoVisualizar)
+function remover_clase(selector_css,clase){
+	Array.from(document.querySelectorAll(selector_css)).map(x=>x.classList.remove(clase))
+}
+function agregar_clase(selector_css,clase){
+	Array.from(document.querySelectorAll(selector_css)).map(x=>x.classList.add   (clase))
+}
+function ocultar_contenido(id){
+	if(id!=undefined){
+		remover_clase("#"+id,"mostrado")
+		agregar_clase("#"+id,"oculto")
+	}else{
+		remover_clase(".contenido","mostrado")
+		agregar_clase(".contenido","oculto")
+	}
+}
+function mostrar_contenido(id){
+	if(id!=undefined){
+		remover_clase("#"+id,"oculto")
+		agregar_clase("#"+id,"mostrado")
+	}else{
+		remover_clase(".contenido","oculto")
+		agregar_clase(".contenido","mostrado")
+	}
+}
+﻿function cambiar_visibilidad_contenido(contenido){
+	var id = contenido.id
+	var condición_1 = contenido.classList.contains("oculto")
+	var condición_2 = contenido.classList.contains("mostrado")
+	if(condición_1&&!condición_2){
+		ocultar_contenido()
+		mostrar_contenido(id)
+	}else{
+		if(!condición_1&&condición_2){
+			ocultar_contenido(id)
+		}
+	}
+}
+function mostrar(esto)
 {
-	var objeto=document.all[objetoVisualizar]
-	var estilo=objeto.style
-	var muestra=estilo.display
-	for(var i=1;i<=3;i++)
-	{
-		document.all["mostrar"+i].style.display='none'
-	}
-	mostrar2.innerHTML=""
-	if(muestra=="none")
-	{
-		if(objetoVisualizar=="mostrar1")
-		{
-			objeto.innerHTML=generarVersiones()
+	var id = +esto.id.slice(1)
+	var nombre_de_contenido = "#mostrar"+id
+	var contenido = document.querySelector(nombre_de_contenido)
+	var está_cargado = contenido.classList.contains("cargado")
+	cambiar_visibilidad_contenido(contenido,id)
+	if(!está_cargado){
+		switch(id){
+			case 1:
+				contenido.innerHTML=generarVersiones()
+			;break;case 2:
+				contenido.innerHTML="<div>Mis videos de Youtube</div><div>"+generarYoutube()+"</div>"
+			;break;case 3:
+				contenido.innerHTML="<div>"+generarProgramación()+"</div>"
+			;break;
 		}
-		if(objetoVisualizar=="mostrar2")
-		{
-			objeto.innerHTML="<div>Mis videos de Youtube</div><div>"+generarYoutube()+"</div>"
-		}
-		if(objetoVisualizar=="mostrar3")
-		{
-			objeto.innerHTML="<div>"+generarProgramación()+"</div>"
-		}
-		estilo.display='block'
-	}
-	else
-	{
-		estilo.display="none"
+		contenido.classList.add("cargado")
 	}
 }
 function cargarVersión()
@@ -35,7 +61,7 @@ function cargarVersión()
 	var salida="Versión actual: "
 	var versión=mostrar1.getElementsByTagName("div")[0].innerHTML.split(" ")[0]
 	var actual=salida+versión
-	V1.innerHTML=actual
+	v1.innerHTML=actual
 }
 function cargarTodo()
 {
@@ -53,3 +79,4 @@ function color(esto,booleano)
 		esto.style.color="FFFFFF"
 	}
 }
+
