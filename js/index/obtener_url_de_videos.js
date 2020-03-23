40663ba6c4fb43793575a7014c7fdd36f5d21fb6
ArtEze <passymas@gmail.com>
2020-03-23 12:33:22 -0300

    Update obtener_url_de_videos.js

--- js/index/obtener_url_de_videos.js
+++ js/index/obtener_url_de_videos.js
@@ -1,13 +1,12 @@
-﻿function obtenerLinksYoutube()
+function obtener_url_de_videos()
 {
-	var y,imagen,linkYoutube,título
-	var x=document.getElementsByClassName("yt-lockup-dismissable")
+	var x = document.getElementsByClassName("yt-lockup-dismissable")
 	var salida=[]
-	for(var i=x.length-1;i>=0;i--)
+	for( var i=x.length-1; i>=0; --i )
 	{
 		if(!x[i].getElementsByClassName("video-unlisted-icon").length)
 		{
-			y=x[i].getElementsByClassName("yt-uix-tile-link")[0]
+			var y=x[i].getElementsByClassName("yt-uix-tile-link")[0]
 			salida[salida.length]={
 				vínculo:y.href.split("=")[1],
 				título:y.title
